f946df609e388eae00d919e969f4360f3aa99df9
toastyhatesthsea <toastyhatesthesea@gmail.com>
2021-01-26 01:58:10 -0800

    initial Commit

---
+++ Quicksorts.java
@@ -0,0 +1,16 @@
+public class Quicksorts
+{
+
+    public static int partition(Comparable[] data, int first, int n)
+    {
+        Comparable pivot = data[first];
+        int tooBigIndex = first + 1;
+        int tooSmallIndex = first + n -1;
+
+
+        
+
+    }
+
+
+}
