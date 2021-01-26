430bd14d3cc0a8eebb4afba75f03f6be72173349
toastyhatesthsea <toastyhatesthesea@gmail.com>
2021-01-26 02:54:49 -0800

    Created Partition method, passes first initial test

--- Quicksorts.java
+++ Quicksorts.java
@@ -7,10 +7,38 @@ public class Quicksorts
         int tooBigIndex = first + 1;
         int tooSmallIndex = first + n -1;
 
+        while (tooBigIndex <= tooSmallIndex)
+        {
+            while (data[tooBigIndex].compareTo(pivot) <= 0 && tooBigIndex <= data.length)
+            {
+                tooBigIndex++;
+            }
+            while (data[tooSmallIndex].compareTo(pivot) >= 0)
+            {
+                tooSmallIndex--;
+            }
+            if (tooBigIndex < tooSmallIndex)
+            {
+                Comparable temp = data[tooBigIndex];
+                data[tooBigIndex] = data[tooSmallIndex];
+                data[tooSmallIndex] = temp;
+            }
+        }
+        data[first] = data[tooSmallIndex];
+        data[tooSmallIndex] = pivot;
 
-        
-
+        return tooSmallIndex;
     }
 
 
+}
+
+class QuickSortTesters
+{
+    public static void main(String[] asasas)
+    {
+        Comparable[] someIntArray = {4, 5, 6, 1, 3, 7, 9, 2, 3, 10, 2};
+
+        Quicksorts.partition(someIntArray, 0, someIntArray.length);
+    }
 }
